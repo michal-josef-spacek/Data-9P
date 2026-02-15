2b5617860653eb391edb315421fe2aeb90428758
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2026-02-15 17:36:26 +0100

    Add new messages

---
+++ 9P/Message/Rauth.pm
@@ -0,0 +1,27 @@
+package Data::9P::Message::Rauth;
+
+use strict;
+use warnings;
+
+use Mo qw(build is);
+use Mo::utils 0.08 qw(check_isa check_required);
+
+extends 'Data::9P::Message';
+
+our $VERSION = 0.01;
+
+has qid => (
+	is => 'ro',
+);
+
+sub BUILD {
+	my $self = shift;
+
+	# Check 'qid'.
+	check_required($self, 'qid');
+	check_isa($self, 'qid', 'Data::9P::Qid');
+
+	return;
+}
+
+1;
---
+++ 9P/Message/Tauth.pm
@@ -0,0 +1,49 @@
+package Data::9P::Message::Tauth;
+
+use strict;
+use warnings;
+
+use Data::9P::Const qw(NOFID);
+use Error::Pure qw(err);
+use Mo qw(build is);
+use Mo::utils qw(check_required);
+use Mo::utils::Number::Range 0.03 qw(check_natural_range);
+
+extends 'Data::9P::Message';
+
+our $VERSION = 0.01;
+
+has afid => (
+	is => 'ro',
+);
+
+has aname => (
+	is => 'ro',
+);
+
+has uname => (
+	is => 'ro',
+);
+
+sub BUILD {
+	my $self = shift;
+
+	# Check 'afid'.
+	check_required($self, 'afid');
+	check_natural_range($self, 'afid', 0, 4294967295);
+	if ($afid == $NOFID) {
+		err "Parameter 'afid' couldn't contain 0xFFFFFFFF value.";
+	}
+
+	# Check 'aname'.
+	check_required($self, 'aname');
+	# TODO Check string, maximum 65535 bytes.
+
+	# Check 'uname'.
+	check_required($self, 'uname');
+	# TODO Check string, maximum 65535 bytes.
+
+	return;
+}
+
+1;
